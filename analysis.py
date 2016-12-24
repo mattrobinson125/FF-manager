fe1d95f55dddae369f695b8742882c6f8aa874af
Matt Robinson <mattrobinson125@gmail.com>
2016-12-24 01:23:50 -0500

    Added team fetching

--- analysis.py
+++ analysis.py
@@ -2,6 +2,10 @@ from bs4 import BeautifulSoup
 import requests
 import re
 
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.95 Safari/537.36'
+}
 class Team:
     def __init__(self, name, tid, owner):
         self.name = name
@@ -17,30 +21,45 @@ class League:
         self.name = name
         #self.teams = []
         self.teams = []
+        self.teams = [None] * 12
         self.players = {99999 : "Dummy Player"}
 
     def printLeague(self):
         for team in self.teams:
             team.printTeam()
 
-    def addTeam(self, newTeam):
-        self.teams.append(newTeam)
+    def addTeam(self, teamId, newTeam):
+        self.teams[teamId-1] = newTeam
 
+    def fetchTeams(self):
+        r  = requests.get("http://games.espn.com/ffl/standings?leagueId=28641&seasonId=2016", headers=headers)
+        soup = BeautifulSoup(r.text, 'html.parser')
 
-if __name__ == "__main__":
-    l = League("My League")
-    l.printLeague()
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.95 Safari/537.36'
-    }
-    r  = requests.get("http://games.espn.com/ffl/standings?leagueId=28641&seasonId=2016", headers=headers)
-    data = r.text
-    soup = BeautifulSoup(data)
+        #Create objects for every team in the league
+        for tr in soup.find_all('tr', class_='tableBody'):
+            teamLink = tr.find('a').get('href') #Link to team
+            rawName = tr.find('a').get('title')
 
-    #Create objects for every team in the league
-    for tr in soup.find_all('tr', class_='tableBody'):
-        teamLink = tr.find('a').get('href') #Link to team
-        rawName = tr.find('a').get('title')
+            teamOwner = re.match(r".*\((.*?)\).*", rawName).group(1)
+            teamName = re.match(r"^(.*?)\(", rawName).group(1)
+            teamId = int(re.match(r".*teamId=([0-9]{1,2}).*", teamLink).group(1))
+
+            newTeam = Team(teamName, teamId, teamOwner)
+            self.addTeam(teamId, newTeam)
+
+    def fetchSchedule(self):
+        for i in range(1,14): #iterate over every week in the regular season schedule
+            url = "http://games.espn.com/ffl/scoreboard?leagueId=28641&matchupPeriodId={}".format(i)
+            r = requests.get(url, headers=headers)
+            soup = BeautifulSoup(r.text, 'html.parser')
+            weekSchedule = []
+            for table in soup.find_all('table', class_='ptsBased matchup'): #every matchup in the week
+                matchup = []
+                for team in table.find_all('tr')[:-1]:
+                    #print('thing:', team.get('id'))
+                    a = re.match(r".*_([0-9]{1,2})_.*", team.get('id')).group(1)
+                    matchup.append(a)
+                weekSchedule.append(matchup)
 
         teamOwner = re.match(r".*\((.*?)\).*", rawName).group(1)
         teamName = re.match(r"^(.*?)\(", rawName).group(1)
@@ -49,4 +68,5 @@ if __name__ == "__main__":
         newTeam = Team(teamName, teamId, teamOwner)
         l.addTeam(newTeam)
 
+    l.fetchTeams()
     l.printLeague()
